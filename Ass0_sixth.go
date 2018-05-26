9be1efc03ace26f832be5786ad3e2d9575b349d6
Andrea Wu <andrea.yh.wu@gmail.com>
2018-05-26 01:32:28 -0400

    Create Ass0_sixth.go

---
+++ Ass0_sixth.go
@@ -0,0 +1,52 @@
+package main
+
+import(
+    "fmt"
+//    "strings"
+//    "bufio"
+    "os"
+    "bytes"
+    "strconv"
+)
+
+func main(){
+    //get command line argument
+    if len(os.Args) != 2{
+        fmt.Printf("invalid number of arguments\n")
+        return
+    }
+    
+    uncompressed_string := os.Args[1]
+    var compressed_string bytes.Buffer
+    
+    //count number of consecutive letters
+    count := 1
+    
+    j := 0
+    i := 1
+    length := len(uncompressed_string)
+    
+    for i < length {
+        if uncompressed_string[i] == uncompressed_string[j] {
+            count++
+        }else{
+            //concat to string yo
+            compressed_string.WriteString(string(uncompressed_string[j]))
+            
+            //strconv.Itoa converts Int to String
+            compressed_string.WriteString(strconv.Itoa(count))
+
+           count = 1
+        }
+
+        j++
+        i++
+    }
+
+    compressed_string.WriteString(string(uncompressed_string[length -1]))
+    compressed_string.WriteString(strconv.Itoa(count))
+    count = 0
+    
+    fmt.Printf("%s\n", compressed_string.String())
+}
+ 
